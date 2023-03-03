3a86789d39c72932c74c7b26eb786eb3a02af08c
Peter Jankuliak <p.jankuliak@gmail.com>
2023-03-03 16:04:17 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+# Directories used in this test
+rm -rf /tmp/.tmp*
+rm -rf /tmp/ouisync-test-*
+
+function date_tag {
+    date +"%Y-%m-%d--%H-%M-%S"
+}
+
+echo "$(date_tag) Compiling the test"
+cargo build
+
+# This next one will not compile again, we need it to get the executable name.
+exe=./target/debug/database-bug
+
+dir=`mktemp --tmpdir -d ouisync-test-XXXXXX`
+n=200
+
+echo "$(date_tag) Working in directory $dir"
+echo "$(date_tag) Starting $n tests"
+
+for i in $(seq $n); do
+    while true; do
+        $exe 2>&1
+        r=$?
+        if [ "$r" -ne "0" ]; then break; fi
+    done > $dir/test-$i.log 2>&1 &
+
+    pids[${i}]=$!
+done
+
+aborted_process="";
+
+echo "$(date_tag) Awaiting first process to fail"
+
+while [ -z "$aborted_process" ]; do
+    for i in $(seq $n); do
+        pid=${pids[${i}]}
+        if ! ps $pid > /dev/null; then
+            echo "Process $i (pid:$pid) aborted"
+            aborted_process=$i
+            break;
+        fi
+    done
+    sleep 0.5
+done
+
+echo "$(date_tag) Killing rest of the jobs:"
+
+for i in $(seq $n); do
+    pid=${pids[${i}]}
+    if [ $i -ne $aborted_process ]; then
+        echo "  killing job:$i with pid:$pid"
+        pkill -P $pid 2>/dev/null 1>&2 & # || true 
+        rm $dir/test-$i.log
+    fi
+done
+
+new_log_name=/tmp/ouisync-log-$(date +"%Y-%m-%d--%H-%M-%S").txt
+mv $dir/test-$aborted_process.log $new_log_name
+echo "$(date_tag) Log saved to $new_log_name"
+
+vim $new_log_name
---
+++ src/main.rs
@@ -0,0 +1,166 @@
+use sqlx::{
+    sqlite::{
+        Sqlite, SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions, SqliteSynchronous,
+    },
+    Row, SqlitePool,
+};
+use std::{io::Error, path::Path};
+use tempfile::TempDir;
+use tokio::fs;
+
+pub(crate) struct Pool {
+    // Pool with a single read-only connection.
+    reads: SqlitePool,
+    // Pool with a single writable connection.
+    write: SqlitePool,
+}
+
+impl Pool {
+    async fn create(connect_options: SqliteConnectOptions) -> Result<Self, sqlx::Error> {
+        let common_options = connect_options
+            //.journal_mode(SqliteJournalMode::Wal)
+            .synchronous(SqliteSynchronous::Normal)
+            .pragma("recursive_triggers", "ON");
+
+        let write_options = common_options.clone();
+        let write = SqlitePoolOptions::new()
+            .min_connections(1)
+            .max_connections(1)
+            .test_before_acquire(false)
+            .connect_with(write_options)
+            .await?;
+
+        let read_options = common_options.read_only(true);
+        let reads = SqlitePoolOptions::new()
+            .min_connections(1)
+            .max_connections(1)
+            .test_before_acquire(false)
+            .connect_with(read_options)
+            .await?;
+
+        Ok(Self { reads, write })
+    }
+
+    pub(crate) async fn close(&self) -> Result<(), sqlx::Error> {
+        self.write.close().await;
+        self.reads.close().await;
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    for run_i in 0..1000 {
+        database_commit_consistency(run_i).await;
+    }
+}
+
+async fn database_commit_consistency(run_i: u32) {
+    use futures_util::TryStreamExt;
+    use tokio::select;
+
+    let (_a_base_dir, a_pool) = create_temp_db().await.unwrap();
+    let (_b_base_dir, b_pool) = create_temp_db().await.unwrap();
+
+    println!("START");
+    {
+        let mut tx = a_pool.write.begin().await.unwrap();
+        sqlx::query("CREATE TABLE a_table (id INTEGER PRIMARY KEY)")
+            .execute(&mut *tx)
+            .await
+            .unwrap();
+        tx.commit().await.unwrap();
+    }
+
+    {
+        let mut tx = b_pool.write.begin().await.unwrap();
+        sqlx::query("CREATE TABLE b_table (id INTEGER PRIMARY KEY)")
+            .execute(&mut *tx)
+            .await
+            .unwrap();
+        tx.commit().await.unwrap();
+    }
+
+    let highest_id = 200;
+
+    {
+        let mut tx = a_pool.write.begin().await.unwrap();
+
+        for i in 0..(highest_id + 1) {
+            let _id = sqlx::query(
+                "INSERT INTO a_table (id)
+                 VALUES (?)
+                 RETURNING id",
+            )
+            .bind(&(i as i64))
+            .map(|row: sqlx::sqlite::SqliteRow| row.get::<u32, usize>(0))
+            .fetch_one(&mut *tx)
+            .await
+            .unwrap();
+        }
+
+        tx.commit().await.unwrap();
+    }
+
+    select! {
+        _ = async {
+            loop {
+                let mut tx = b_pool.write.begin().await.unwrap();
+                sqlx::query("DELETE FROM b_table")
+                    .execute(&mut *tx)
+                    .await
+                    .unwrap();
+                tx.commit().await.unwrap();
+            }
+        } => {},
+        _ = async {
+            let mut conn = a_pool.reads.acquire().await.unwrap();
+
+            let vec: Vec<u32> = sqlx::query("SELECT id FROM a_table WHERE id = ?")
+                .bind(&(highest_id as i64))
+                .fetch(&mut *conn)
+                .map_ok(|row| row.get::<u32, usize>(0))
+                .try_collect()
+                .await
+                .unwrap();
+
+            assert!(!vec.is_empty(), "Failed to retrieve on {}-th iterations", run_i);
+        } => {},
+    }
+
+    a_pool.close().await.unwrap();
+    b_pool.close().await.unwrap();
+}
+
+async fn create_directory(path: &Path) -> Result<(), Error> {
+    if let Some(dir) = path.parent() {
+        fs::create_dir_all(dir).await?
+    }
+
+    Ok(())
+}
+
+async fn create_pool(path: impl AsRef<Path>) -> Result<Pool, std::io::Error> {
+    let path = path.as_ref();
+
+    if fs::metadata(path).await.is_ok() {
+        panic!("Already exists");
+    }
+
+    create_directory(path).await?;
+
+    let connect_options = SqliteConnectOptions::new()
+        .filename(path)
+        .create_if_missing(true);
+
+    let pool = Pool::create(connect_options).await.unwrap();
+
+    Ok(pool)
+}
+
+async fn create_temp_db() -> Result<(TempDir, Pool), std::io::Error> {
+    let temp_dir = TempDir::new()?;
+    let pool = create_pool(temp_dir.path().join("temp.db")).await?;
+
+    Ok((temp_dir, pool))
+}
